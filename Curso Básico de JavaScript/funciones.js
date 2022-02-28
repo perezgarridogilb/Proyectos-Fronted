314c694313b73d74e9659621446ad8ea954936b3
Gilberto <Betho6990@gmail.com>
2022-02-28 15:17:06 -0600

    One Section of Java Script Course

---
+++ Curso Básico de JavaScript/funciones.js
@@ -0,0 +1,17 @@
+/* Declarativas */
+
+function miFuncion() {
+    return 3;
+}
+
+miFuncion();
+
+/* Expresión */
+/* Hay una función en esa variable */
+/* Anónima y sin nombre */
+
+var miFuncion = function(a, b) {
+    return a + b;
+}
+
+miFuncion();
---
+++ Curso Básico de JavaScript/valores.js
@@ -0,0 +1,23 @@
+/* var: Representación del lugar donde va a haber valores */
+var nombre = "Oscar";
+
+// declarar
+var edad;
+
+edad
+
+// inicializar
+edad = 30;
+
+edad
+
+// arreglo de cadenas de texto
+var elementos = ["Computadora","Celular"];
+
+// objeto
+var persona = {
+    nombre: "Diego", 
+    edad: 30
+}
+
+persona;
