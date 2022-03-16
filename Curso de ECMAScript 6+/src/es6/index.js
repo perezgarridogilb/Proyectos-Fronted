439da1f71fa517146c16c2eb65f5c3dd7cbdf49b
Gilberto <Betho6990@gmail.com>
2022-03-15 21:04:49 -0600

    Penultimate

--- Curso de ECMAScript 6+/src/es6/index.js
+++ Curso de ECMAScript 6+/src/es6/index.js
@@ -91,8 +91,51 @@ const names = [
 
 ] 
 // func anónima
+// item: elemento que quiero traer
 let listOfNames = names.map(function(item) { 
     console.log(item.name); 
 }) 
 
+// =>: función anónima que va a retornar algo
 let listOfNames1 = names.map(item => console.log(item.name)); 
+
+// =>: arroy functions 
+// const listOfNames2 = (name, age, country) => { 
+//     ...
+// }
+// 
+// const listOfNames4 = name => { 
+//     ...
+// } 
+
+// generando una función para pasarle el valor de num de la operación num * num
+const square = num => num * num; 
+
+// const helloPromise = () => { 
+//     return new Promise((resolve, reject) => { 
+//         if(true) { 
+//             resolve('Promesas')
+//         } else {
+//             reject('None');
+//         }
+//     });
+// }
+// 
+// helloPromise()
+//     .then(response => console.log(response)) 
+//     .then(() => console.log('hola'))
+//     .catch(error => console.log(error)) 
+
+const helloPromise = () => { 
+    return new Promise((resolve, reject) => { 
+        if(false) { 
+            resolve('Promesas')
+        } else {
+            reject('None');
+        }
+    });
+}
+
+helloPromise()
+    .then(response => console.log(response)) 
+    .catch(error => console.log(error)) 
